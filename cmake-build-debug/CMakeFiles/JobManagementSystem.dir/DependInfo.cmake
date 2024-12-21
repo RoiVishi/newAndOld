b5af5887490e7845af98a9efe1576a567828f0a0
RoiVishi <roiwi@ac.sce.ac.il>
2024-12-21 18:07:37 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/JobManagementSystem.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/Application.cpp" "CMakeFiles/JobManagementSystem.dir/Application.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/Application.cpp.o.d"
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/Candidate.cpp" "CMakeFiles/JobManagementSystem.dir/Candidate.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/Candidate.cpp.o.d"
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/Employer.cpp" "CMakeFiles/JobManagementSystem.dir/Employer.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/Employer.cpp.o.d"
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/Jobs.cpp" "CMakeFiles/JobManagementSystem.dir/Jobs.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/Jobs.cpp.o.d"
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/User.cpp" "CMakeFiles/JobManagementSystem.dir/User.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/User.cpp.o.d"
+  "/Users/roiwishengrad/untitled25/JobManagementSystem/main.cpp" "CMakeFiles/JobManagementSystem.dir/main.cpp.o" "gcc" "CMakeFiles/JobManagementSystem.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/JobManagementSystem.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/JobManagementSystem.dir/Application.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/Application.cpp.o.d"
+  "CMakeFiles/JobManagementSystem.dir/Candidate.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/Candidate.cpp.o.d"
+  "CMakeFiles/JobManagementSystem.dir/Employer.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/Employer.cpp.o.d"
+  "CMakeFiles/JobManagementSystem.dir/Jobs.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/Jobs.cpp.o.d"
+  "CMakeFiles/JobManagementSystem.dir/User.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/User.cpp.o.d"
+  "CMakeFiles/JobManagementSystem.dir/main.cpp.o"
+  "CMakeFiles/JobManagementSystem.dir/main.cpp.o.d"
+  "JobManagementSystem"
+  "JobManagementSystem.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/JobManagementSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
